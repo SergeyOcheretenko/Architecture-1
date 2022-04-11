81b406aa28691f80bcead30fd46e9bb020175705
juliion <juliaionova111@gmail.com>
2022-04-11 16:56:19 +0300

    feat: add getting and formatting time

--- server.go
+++ server.go
@@ -1,10 +1,14 @@
 package main
-
-import "net/http"
  
-func main() {
-   http.HandleFunc("/test", func(rw http.ResponseWriter, r *http.Request) {
-       rw.Write([]byte("work"))
+import (
+   "net/http"
+   "time"
+)
+ func main() {
+   http.HandleFunc("/time", func(rw http.ResponseWriter, r *http.Request) {
+       now := time.Now()
+       formattedTime := now.Format("2006-01-02T15:04:05Z07:00")
+       rw.Write([]byte(formattedTime))
    })
    http.ListenAndServe(":8795", nil)
 }
