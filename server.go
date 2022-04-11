767f56d1a6d86c8fa85b4466f220d531721cffc4
SergeyOcheretenko <ocheretenko.s@gmail.com>
2022-04-11 17:16:06 +0300

    feat: add JSON encoding

--- server.go
+++ server.go
@@ -1,24 +1,26 @@
 package main
-
+ 
 import (
   "net/http"
   "time"
-  "fmt"
+  "encoding/json"
 )
-
+ 
 type Time struct {
   NowTime string
 }
-
+ 
 func main() {
   const RFC3339 = "2006-01-02T15:04:05Z07:00"
-
+ 
   http.HandleFunc("/time", func(rw http.ResponseWriter, r *http.Request) {
     now := time.Now()
     formattedTime := now.Format(RFC3339)
+ 
     structTime := Time{NowTime: formattedTime}
-    fmt.Println(structTime)
-    rw.Write([]byte(formattedTime))
+ 
+    data, _ := json.MarshalIndent(structTime, "", "\t")
+    rw.Write(data)
   })
   http.ListenAndServe(":8795", nil)
 }
