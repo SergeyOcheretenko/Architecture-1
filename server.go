71b97620428754efd1ac05809394583e033eec88
Misha <sokolyukmisha@gmail.com>
2022-04-11 17:02:58 +0300

    feat: separate RFC3339 constant

--- server.go
+++ server.go
@@ -10,12 +10,14 @@ type Time struct {
   NowTime string
 }
 func main() {
+  const RFC3339 = "2006-01-02T15:04:05Z07:00"
+
   http.HandleFunc("/time", func(rw http.ResponseWriter, r *http.Request) {
     now := time.Now()
-    formattedTime := now.Format("2006-01-02T15:04:05Z07:00")
+    formattedTime := now.Format(RFC3339)
     structTime := Time{NowTime: formattedTime}
     fmt.Println(structTime)
-    rw.Write([]byte("work"))
+    rw.Write([]byte(formattedTime))
   })
   http.ListenAndServe(":8795", nil)
 }
