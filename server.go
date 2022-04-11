fb3608ca6c856b8100f10db84eb4073be5cb45af
Misha <sokolyukmisha@gmail.com>
2022-04-11 17:01:36 +0300

    feat: add Time struct

--- server.go
+++ server.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "net/http"
+  "time"
+  "fmt"
+)
 
+type Time struct {
+  NowTime string
+}
 func main() {
-  fmt.Println("test") 
+  http.HandleFunc("/time", func(rw http.ResponseWriter, r *http.Request) {
+    now := time.Now()
+    formattedTime := now.Format("2006-01-02T15:04:05Z07:00")
+    structTime := Time{NowTime: formattedTime}
+    fmt.Println(structTime)
+    rw.Write([]byte("work"))
+  })
+  http.ListenAndServe(":8795", nil)
 }
